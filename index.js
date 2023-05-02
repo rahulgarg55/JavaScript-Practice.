2f623f38a6f031f3c710cf6d7e9a19eb680d77b8
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2023-05-02 10:52:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/**
- * @param {number[]} salary
- * @return {number}
- */
 var average = function(salary) {
       const max = Math.max(...salary)
     const min = Math.min(...salary)
@@ -20,3 +16,293 @@ var average = function(salary) {
 };
 
 -------------------------------------------------------------------------------------
+      
+      
+      
+      
+      
+      
+      <html>
+  <body>
+<script>
+var ary=new Array();
+ary[0]=10;
+ary[1]=true;
+ary[2]="rahul";
+  for(var a=0;a<3;a++){
+    document.write("<li>"+ary[a]+"</li>");
+  }
+  </script>
+  </body>
+  </html>
+
+
+
+
+
+
+
+
+We can print all values using for in loop means all the properities
+<html>
+  <body>
+<script>
+var obj={
+firstname:"rahul",
+lastname:"garg",
+age:25,
+email:"gargr0109@gmail.com"
+};
+for(var key in obj){
+document.write(obj[key]+"<br>");
+}
+  </script>
+  </body>
+  </html>=
+  
+  
+  
+  
+  
+  
+  
+  
+  includes is a case sensititve function 
+  
+  
+  var a=str.includes("ipt");
+  
+  var a=str.endswith("age");
+  
+  
+  var str="javascript is a great language";
+  var a=str.search("is");   // 11
+  var a=str.search("great");   //16
+  
+  
+  
+  
+  
+  
+  match()   javascript string method
+  var a = str.match(/is/g);  g means globally will search again and again
+  
+
+
+
+
+
+
+
+indexof() // return index and works like search of first occurence
+lastindexof()  //returns index from last 
+
+
+
+
+
+
+
+replace()  //replaces word
+var a=str.replace("javascript", "php");
+to replace globally
+var a=str.replace(/is/g,"are"); // it will replace is globally with are
+
+
+
+
+
+
+
+
+
+
+trim()
+var a=str.trim();
+alert(a); // that removes whitespace characters from both the beginning and the end of a string.
+
+
+
+
+
+
+
+
+var str=("javaxecript");
+alert(str)
+
+
+
+
+
+
+
+charAt()
+var a=str.charAt(3); //it will return character at index we have passed.
+
+
+
+
+
+
+
+charCodeAt()
+var a=str.charCodeAt(1);  //it will return ASCII code of char at index 1
+
+
+fromCharCode(110);
+
+
+
+
+concat() // to concat more than one strings.
+var str="rahul";
+var str1="garg";
+var ans=str.concat(str1);
+document.write(ans);
+
+
+
+
+
+
+split()
+var a=str.split(" "); //it will split words at every space.(means whatever we will pass)
+
+
+
+
+repeat()
+var a=str.repeat(2);  //string will get repeated 2 times now
+
+
+
+
+slice()
+var a=str.slice(3); it will return  string from character 3
+
+
+
+
+var a=str.slice(-2) //it will return 2 indexes from last.
+
+
+
+substr()
+var a=str.substr(2);  it will also return string from index 2
+
+
+
+
+tostring()  //it will convert anything to string 
+var str=50;
+var a=str.tostring();  will conver
+document.write(a+20);   // OUTPUT will be 5020
+
+
+
+
+
+valueof()
+
+
+
+
+Number Methods.
+var a="99";
+var num=Number(a);
+document.write(num);  //now 99 will be integer
+
+
+
+
+
+
+parseint()
+
+var a="99 88 77";
+var num=parseInt(a);
+document.write(num);  //will return first integer value   
+
+var a="10.00";
+var num=parseInt(a);
+document.write(num);  //will return value without decimal
+
+
+
+
+parsefloat()
+var num=parseFloat(a); // it will also return vlaue after dots.
+
+
+
+isFinite() //will return true if num is finite
+var a=100;
+var num=isfinite(a);  //true
+
+
+
+
+
+var a=10.50;
+var num=isInteger(a); //false
+
+
+
+var num=a.toFixed(num);  
+var num=a.toPrecision(num); 
+-------------------------------------------------------------------------------------
+
+
+Math.ceil(1.2); //2
+Math.floor(1.2); //1
+Math.round(0.60);
+Math.trunc(2.49);  //it will simply remove value after dot.
+Math.sqrt(64); //8
+pow(x,y);   pow(4,3);   //64
+random();      // will return random values everytime we refresh
+var a=math.abs(-5.25);
+var a=Math.PI;   
+----------------------------------------------------------------------------------------
+
+JS Date Object
+toDateString()
+var now = new Date();
+document.write(now.toDateString());  // to get in readable form
+document.write(now.getFullYear()); 
+document.write(now.getDay()); 
+document.write(now.getMonth());
+getHours();
+getMinutes();
+getSeconds();
+getMilliSeconds();
+
+setDate();
+now.setDate(20);
+now.setFullYear();
+now.setMonth();
+now.setHours();
+---------------------------------------------------------------------------------------------
+Document Object Model
+
+
+Targeting Dom object.
+
+ID      -> document.getElementbyId(id);
+Class     -> document.getElementByClassNmae(name)
+Tag Name    ->document.getElementByTagName(name)
+
+
+var element;
+element=document.all;   //it will return all tags in array type of the page.
+console.log(element);
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+
+
+
+
+------------------------------------------------------------------------------------------------------
+
